01a25cbe2f590ba91032c7ba849e48c79e1705f3
kchorzelewski <115093050+kchorzelewski@users.noreply.github.com>
2023-02-24 11:30:31 +0100

    Create Lekcja 1.py

---
+++ Lekcja 1.py
@@ -0,0 +1,32 @@
+def zad1():
+    print("*******************")
+    print("*******************")
+    print("*******************")
+    print("*******************")
+def zad2():
+    print("*****************")
+    print("*               *")
+    print("*               *")
+    print("*****************")
+def zad3():
+    print("*")
+    print("**")
+    print("***")
+    print("****")
+def zad4():
+    print((512-282)/(47*48+5))
+def zad5():
+    x = 7
+    for i in range(1,5):
+        print(i*x, end='---')
+    print(x*5)
+def zad6():
+    lipsum = "Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, jak Aldus PageMaker"
+def zad7():
+    a = eval(input())
+    b = eval(input())
+    print(a*b)
+def zad8():
+    x = eval(input())
+    x = x*2.2
+    print(x)
