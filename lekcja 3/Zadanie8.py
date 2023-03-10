f70badf5a90065e229f78e8b595c50c1501dc4de
kchorzelewski <115093050+kchorzelewski@users.noreply.github.com>
2023-03-10 12:21:57 +0100

    Create Zadanie8.py

---
+++ lekcja 3/Zadanie8.py
@@ -0,0 +1,16 @@
+def multipli_game():
+    wrong = 0
+    good = 0
+    for i in range(10):
+        a = random.randint(1, 9)
+        b = random.randint(1, 9)
+        x = eval(input(f"Pytanie {i+1}: {a} x {b} = "))
+        if x == a*b:
+            print("Poprawna odpowiedź!")
+            good += 1
+        else:
+            print(f"Błędna odpowiedź, poprawną odpowiedzia jest {a*b}")
+            wrong += 1
+    print(f"Błędne odpowiedzi: {wrong}")
+    print(f"Poprawne odpowiedzi: {good}")
+multipli_game()
