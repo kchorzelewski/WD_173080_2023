16a1a16cb8b81f8e6abbac22514a70f374be7713
kchorzelewski <115093050+kchorzelewski@users.noreply.github.com>
2023-03-10 12:17:41 +0100

    Create Zadanie1.py

---
+++ lekcja 3/Zadanie1.py
@@ -0,0 +1,8 @@
+def Zadanie1(a):
+    if a > 0:
+        print("funkcja jest rosnąca")
+    elif a < 0:
+        print("funkcja jest malejąca")
+    else:
+        print("funkcja jest stała")
+Zadanie1(2)
