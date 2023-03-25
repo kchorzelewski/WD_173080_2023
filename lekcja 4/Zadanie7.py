d412c8585c904672c6319ec0593bd13fd9f396de
kchorzelewski <115093050+kchorzelewski@users.noreply.github.com>
2023-03-25 19:34:05 +0100

    Update Zadanie7.py

--- lekcja 4/Zadanie7.py
+++ lekcja 4/Zadanie7.py
@@ -1,6 +1,14 @@
 import numpy as np
 
-def func():
-    x = np.array([np.arange(2,7,2),np.arange(3)],np.arange(7,-2,2))
+
+def func(n):
+    scalar = 0
+    x = np.diag(np.ones(n)*2)
+    for i in range(4,n*2+1,2):
+        n-=1
+        scalar+=1
+        x = x + np.diag(np.ones(n)*i, scalar)
+        x = x + np.diag(np.ones(n)*i, scalar*(-1))
     print(x)
-func()
+
+func(4)
