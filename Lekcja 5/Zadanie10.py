5e2f7641b9839fc8f34eecd60c6b3cc5f1ade273
kchorzelewski <115093050+kchorzelewski@users.noreply.github.com>
2023-03-26 17:56:00 +0200

    Create Zadanie10.py

---
+++ Lekcja 5/Zadanie10.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+x = np.arange(81).reshape(9,9)
+
+print("możemy zmienić wektor na macierz 9x9 tylko wtedy, kiedy miejsca nam na to pozwalają. Nie może być za mało ani za dużo")
