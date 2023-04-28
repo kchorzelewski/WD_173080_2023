f2296384ae75507fc87195248b4ab2aeed9602f5
kchorzelewski <115093050+kchorzelewski@users.noreply.github.com>
2023-04-28 12:18:50 +0200

    Update zadanie4.py

--- lekcja8/zadanie4.py
+++ lekcja8/zadanie4.py
@@ -3,9 +3,9 @@ import numpy as np
 
 
 x = np.arange(0,30,0.1)
-y = np.arange(1.5,31.5,0.1)
-plt.plot(np.sin(x)+2)
-plt.plot(np.cos(y))
-plt.xticks()
-plt.xlabel(np.arange(10))
+y = np.arange(3.2,33.2,0.1)
+plt.title("Wykres sin(x), sin(x)")
+plt.plot(np.sin(x)+2, label="sin(x)")
+plt.plot(np.sin(y), label="sin(x)")
+plt.legend()
 plt.show()
