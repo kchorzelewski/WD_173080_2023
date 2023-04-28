8d2138757d9bba751477a0aee39a19fa8d4a4cde
kchorzelewski <115093050+kchorzelewski@users.noreply.github.com>
2023-04-28 11:54:31 +0200

    Update zadanie1.py

--- lekcja8/zadanie1.py
+++ lekcja8/zadanie1.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-plt.plot(1/np.arange(21))
+plt.plot(1/np.arange(21), label="f(x) = 1/x")
 plt.ylabel("f(x)")
 plt.xlabel('x')
+plt.legend()
 plt.axis([0,20,0,1])
 plt.show()
