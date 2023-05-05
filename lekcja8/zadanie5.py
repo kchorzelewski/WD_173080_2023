039da8d2a2d9c70f07acd92bcbec749bc561268c
kchorzelewski <115093050+kchorzelewski@users.noreply.github.com>
2023-05-05 12:42:35 +0200

    Update zadanie5.py

--- lekcja8/zadanie5.py
+++ lekcja8/zadanie5.py
@@ -2,21 +2,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-# df = pd.read_csv("iris.data", header=None)
-# print(df)
+df = pd.read_csv('iris.data', delimiter=',', header=None)
 
-data ={'a':np.arange(50),# 1, 2, 3, ..., 48, 49
-       'c':np.random.randint(0,50,50),
-       'd':np.random.randn(50)}
-data['b']=data['a']+10*np.random.randn(50) # dodanie nowego klucza ‘b’
-data['d']=np.abs(data['d'])*100 # nadpisanie wartości klucza ‘d’
 
-plt.scatter('a','b',c='c',s='d',data=data)
-plt.xlabel('wartość a')
-plt.ylabel('wartość b')
+x = df[0]
+y = df[1]
+c = np.random.randint(0, 50, len(df[0]))
+s = np.zeros(len(df[0]))
+
+for i in range(len(s)):
+    s[i] = np.abs(x[i] - y[i])*5
+
+plt.scatter(x, y, c=c, s=s)
+plt.xlabel('sepal length')
+plt.ylabel('sepal width')
 plt.show()
-# x = np.arange(0,30,0.1)
-# plt.title("Wykres sin(x), cos(x)")
-# plt.plot(np.cos(x), label="cos(x)")
-# plt.xlabel("x")
-# plt.ylabel("sin(x), cos(x)")
